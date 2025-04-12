b12ec9ad978533caf6ea7432970b9b58f57f774b
Prakhar Sachan <sachan2004prakhar@gmail.com>
2025-04-12 11:19:21 +0530

    fix small issue -4

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,16 +1,15 @@
-
-import { LuBrain } from "react-icons/lu";
 import { useAuth } from "../context/AuthContext";
 import Button from "./Button";
+import { FaRegNoteSticky } from "react-icons/fa6";
 
 function NavBar() {
   const { user, logout } = useAuth();
   return (
-    <div className="w-full flex items-center justify-between bg-purple-200 px-8 py-4">
+    <div className="w-full flex items-center justify-between bg-purple-200 px-4 py-4">
       <div className="flex items-center gap-3">
-        <LuBrain className="size-10 text-purple-600" />
+        <FaRegNoteSticky className="size-10 text-purple-600" />
         <p className="text-xl font-semibold text-purple-600 hidden sm:inline-block">
-          Second Brain
+          Nauti
         </p>
       </div>
       <div className="flex gap-5">
