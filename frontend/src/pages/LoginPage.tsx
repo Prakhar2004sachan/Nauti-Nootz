47117b96c25741688ea51598356fdcd7d84d127d
Prakhar Sachan <sachan2004prakhar@gmail.com>
2025-04-12 22:28:31 +0530

    login fix

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,7 +1,18 @@
 
+import { useEffect } from "react";
 import Login from "./Login";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
 
 function LoginPage() {
+  const { isAuthenticated, loading } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!loading && isAuthenticated) {
+      navigate("/");
+    }
+  }, [isAuthenticated, loading, navigate]);
   return (
     <div className="w-full h-screen bg-gray-100 flex items-center justify-center flex-col">
       <p className="text-4xl">Hello, Dear User</p>
