4ff61afaacc5aa78552f055579fc3d5471decef1
Prakhar Sachan <sachan2004prakhar@gmail.com>
2025-04-12 09:48:22 +0530

    still fixing -1

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": "https://nauti-nootz-be.vercel.app",
+      "/api": {
+        target: "https://nauti-nootz-be.vercel.app",
+        changeOrigin: true,
+        secure: false,
+      },
     },
     host: "0.0.0.0",
     fs: {
